89609198ddaf53bfb5ee213a2b2d93cb013d8e7e
Svyatoslav Oprysko <opryskosv@gmail.com>
2019-03-30 13:34:09 +0200

    Added basic controller layer.

---
+++ src/main/java/com/site/backend/controller/AnimeController.java
@@ -0,0 +1,42 @@
+package com.site.backend.controller;
+
+import com.site.backend.domain.Anime;
+import com.site.backend.service.AnimeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/anime")
+public class AnimeController {
+
+    private final AnimeService animeService;
+
+    @Autowired
+    public AnimeController(AnimeService animeService) {
+        this.animeService = animeService;
+    }
+
+    @GetMapping("/")
+    public Iterable getAll() {
+        return animeService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Anime getAnimeById(@PathVariable Long id) {
+        return animeService.getAnimeById(id);
+    }
+
+    @PostMapping("/create")
+    public void addNewAnime(@RequestBody Anime newAnime) {
+        animeService.createNewAnime(newAnime);
+    }
+
+    @PutMapping("/update")
+    public void updateAnime(@RequestBody Anime updatedAnime) {
+        animeService.updateAnime(updatedAnime);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteAnimById(@PathVariable Long id) {
+        animeService.deleteAnimeById(id);
+    }
+}
---
+++ src/main/java/com/site/backend/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.site.backend.controller;
+
+import com.site.backend.domain.User;
+import com.site.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public Iterable<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long userId) {
+        return userService.getUserById(userId);
+    }
+
+    @PostMapping("/create")
+    public void addNewUser(@RequestBody User newUser) {
+        userService.createUser(newUser);
+    }
+
+    @PutMapping("/update")
+    public void updateUser(@RequestBody User updatedUser) {
+        userService.updateUser(updatedUser);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUserById(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+}
